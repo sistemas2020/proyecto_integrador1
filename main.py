78c89fc6d7d957c00f01933df34367f6fab02573
Esteban <eebh98@hotmail.com>
2023-11-16 23:47:25 -0500

    segundo commit: Agrega readchar

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 nombre_jugador = input("Por favor, introduce tu nombre: ")
 
 print(f"Bienvenido, {nombre_jugador}!")
+import readchar
+# Bucle infinito
+while True:
+    # Lee un carácter desde la entrada
+    tecla = readchar.readkey()
+
+    # Imprime el carácter leído
+    print(f"Tecla presionada: {tecla}")
+
+    # Comprueba si la tecla presionada es la tecla de flecha arriba ("↑")
+    if tecla == '↑':
+        print("Se presionó la tecla de flecha arriba. Saliendo del bucle.")
+        break
