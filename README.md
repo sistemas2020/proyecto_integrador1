4eb97f15bc984bf698ac7dd7a65e324e2b3fcf62
Esteban <eebh98@hotmail.com>
2023-11-23 00:07:19 -0500

    parte 4

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # proyecto_integrador1
+Laberinto Aventura - Parte 1
+Descripción
+Bienvenido a la emocionante primera parte de Laberinto Aventura. Este proyecto de curso te sumergirá en el mundo de los videojuegos de texto y te desafiará a explorar intrincados laberintos representados por caracteres ASCII. En este primer capítulo, aprenderás los conceptos fundamentales de la creación de un juego de laberinto interactivo.
+
+Características Principales
+Laberintos Desafiantes: Enfrentarás laberintos generados aleatoriamente, donde cada # representa una pared impenetrable, . es un pasillo que puedes recorrer y P es tu personaje, listo para ser guiado por ti.
+
+Movimiento Intuitivo: Utiliza las teclas de flecha ↑, ↓, ← y → de tu teclado para moverte por el laberinto y encontrar la salida. ¡Ten cuidado con las paredes!
+
+Aprendizaje Interactivo
+Este proyecto te permitirá adquirir conocimientos prácticos en programación y desarrollo de videojuegos. A medida que avanzas, aprenderás a trabajar con estructuras de datos, algoritmos de búsqueda y entrada del usuario para crear una experiencia de juego fluida y envolvente.
+
+Objetivos de la Parte 1
+Crear un generador de laberintos con caracteres ASCII.
+Implementar un sistema de movimiento para el personaje utilizando las teclas de flecha.
+Establecer reglas para la interacción del personaje con las paredes y los pasillos.
+Proporcionar una experiencia de juego básica pero emocionante para el jugador.
+Preparación para Parte 2
+La Parte 1 servirá como base sólida para la segunda parte de este proyecto, donde agregaremos características más avanzadas, como enemigos, elementos recolectables y objetivos de juego específicos.
+
+¡Prepárate para adentrarte en un emocionante mundo de laberintos y desafíos!
+
+Este proyecto será una oportunidad emocionante para desarrollar tus habilidades de programación y diseño de juegos mientras creas una experiencia de juego única y divertida. ¡Comienza tu aventura en el mundo de los laberintos hoy mismo!
--- main.py
+++ main.py
@@ -2,3 +2,54 @@
 nombre_jugador = input("Por favor, introduce tu nombre: ")
 
 print(f"Bienvenido, {nombre_jugador}!")
+import os
+from typing import List, Tuple
+
+def generar_laberinto(size: int) -> List[List[str]]:
+    # Función para generar un laberinto utilizando el generador de la página proporcionada
+    # El laberinto se representa como una matriz de caracteres
+    # Utiliza # para las paredes y . para los caminos
+    laberinto = [["#" if i % 2 == 0 and j % 2 == 0 else "." for j in range(size)] for i in range(size)]
+    return laberinto
+
+def mostrar_laberinto(laberinto: List[List[str]]) -> None:
+    # Función para mostrar el laberinto en la pantalla
+    for fila in laberinto:
+        print("".join(fila))
+
+def limpiar_pantalla() -> None:
+    # Función para limpiar la pantalla
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main_loop(laberinto: List[List[str]], inicio: Tuple[int, int], final: Tuple[int, int]) -> None:
+    # Función principal que maneja el bucle del juego
+    px, py = inicio
+
+    while (px, py) != final:
+        limpiar_pantalla()
+        laberinto[py][px] = "P"
+        mostrar_laberinto(laberinto)
+        laberinto[py][px] = "."
+
+        tecla = input("Presiona una tecla de flecha (↑ ↓ ← →) para mover al jugador: ")
+
+        if tecla == '↑' and py > 0 and laberinto[py - 1][px] != "#":
+            py -= 1
+        elif tecla == '↓' and py < len(laberinto) - 1 and laberinto[py + 1][px] != "#":
+            py += 1
+        elif tecla == '←' and px > 0 and laberinto[py][px - 1] != "#":
+            px -= 1
+        elif tecla == '→' and px < len(laberinto[0]) - 1 and laberinto[py][px + 1] != "#":
+            px += 1
+
+if __name__ == "__main__":
+    # Configuración del tamaño del laberinto
+    size = 11
+    # Generar laberinto
+    laberinto = generar_laberinto(size)
+    # Coordenadas de inicio y final
+    inicio = (0, 0)
+    final = (size - 1, size - 1)
+
+    # Ejecutar el bucle principal
+    main_loop(laberinto, inicio, final)
